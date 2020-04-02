bca9a1d36c25812a1f3145559cc459c255806b0c
oimou <s1190013@gmail.com>
2020-04-02 14:17:55 +0900

    Convert a stream to mp3 directly

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 const util = require('util')
 const execFile = util.promisify(require('child_process').execFile)
 const async = require('async')
@@ -6,17 +7,15 @@ const data1 = require("./data1.json").meta
 const data2 = require("./data2.json").meta
 const data = data1.concat(data2)
 
+fs.mkdirSync(path.join(__dirname, "dest"))
+
 async.eachSeries(data, async function (d) {
 	const name = d.title.replace(/ /g, '_')
-	const dst_path = './dest/' + name + '.mp4'
-	const dst_path_mp3 = './dest/' + name + '.mp3'
+	const dst_path_mp3 = path.join(__dirname, 'dest', name + '.mp3')
 
 	console.log(name, d.movie_url.mb_lq)
 
-	if (!fs.existsSync(dst_path)) {
-		await execFile('ffmpeg', ['-i', d.movie_url.mb_lq, '-c', 'copy', '-bsf:a', 'aac_adtstoasc', dst_path])
-	}
 	if (!fs.existsSync(dst_path_mp3)) {
-		await execFile('ffmpeg', ['-i', dst_path, '-acodec', 'libmp3lame', '-ab', '256k', dst_path_mp3])
+		await execFile('ffmpeg', ['-i', d.movie_url.mb_lq, '-acodec', 'libmp3lame', '-ab', '256k', dst_path_mp3])
 	}
 })
